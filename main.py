b9dafcbad60f4a1e924e270ff5a18c3e72b4a93c
Darwinv <nguyenhoangviet290104@gmail.com>
2026-03-27 16:32:13 +0700

    update backend

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import os
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import secrets
+from dotenv import load_dotenv
+load_dotenv()
 
 
 app = FastAPI(title="Drone Delivery Backend")
